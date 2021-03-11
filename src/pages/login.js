4e23da2bf53d3c927d36896ad35003ac102a65be
Lucian Boatright <lucian.boatright@gmail.com>
2021-03-11 18:36:49 +0000

    added login page, images and form holder

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,6 +1,7 @@
 import { useState, useContext, useEffect } from 'react';
 import { useHistory } from 'react-router';
 import FireBaseContext from '../context/firebase';
+import image1 from '../images/watchbox.jpg';
 
 export default function Login() {
   const history = useHistory;
@@ -20,7 +21,12 @@ export default function Login() {
 
   return (
     <div className="container flex mx-auto max-w-screen-md items-center h-screen">
-      <p>OHH LAA DII DAA</p>
+      <div className="flex w-3/5">
+        <img src={image1} alt="Watch box" />
+      </div>
+      <div className="flex flex-col w-2/5">
+        <p>Form holder</p>
+      </div>
     </div>
   );
 }
