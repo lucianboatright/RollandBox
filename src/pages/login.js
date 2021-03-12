8c522c46fe7c70d3007d89db18c96383b5e51849
Lucian Boatright <lucian.boatright@gmail.com>
2021-03-12 14:28:40 +0000

    signup page sorted, added form and sighup button  with css

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,5 +1,6 @@
 import { useState, useContext, useEffect } from 'react';
 import { useHistory } from 'react-router';
+import { Link } from 'react-router-dom';
 import FireBaseContext from '../context/firebase';
 import image1 from '../images/watchbox.jpg';
 
@@ -11,7 +12,7 @@ export default function Login() {
   const [password, setPassword] = useState('');
 
   const [error, setError] = useState('');
-  const inValid = password === '' || emailAddress === '';
+  const isInvalid = password === '' || emailAddress === '';
 
   const handleLogin = () => {};
 
@@ -25,7 +26,45 @@ export default function Login() {
         <img src={image1} alt="Watch box" />
       </div>
       <div className="flex flex-col w-2/5">
-        <p>Form holder</p>
+        <div className="flex flex-col items-center bg-white p-4 border border-grey-primary mb-4 rounded">
+          <h1 className="flex justify-center w-full">
+            <p>RollandBox</p>
+          </h1>
+          {error && <p className="mb-4 text-xs text-red-primary">{error}</p>}
+
+          <form onSubmit={handleLogin} method="POST">
+            <input
+              aria-label="Enter your email address"
+              type="text"
+              placeholder="Email Address"
+              className="text-sm text-grey-base w-full mr-3 py-5 px-4 h-2 border border-grey-primary rounded mb-2"
+              onChange={({ target }) => setEmailAddress(target.value)}
+            />
+            <input
+              aria-label="Enter your password"
+              type="password"
+              placeholder="Password"
+              className="text-sm text-grey-base w-full mr-3 py-5 px-4 h-2 border border-grey-primary rounded mb-2"
+              onChange={({ target }) => setPassword(target.value)}
+            />
+          </form>
+          <button
+            disable={isInvalid}
+            type="submit"
+            className={`bg-blue-500 text-white w-full border border-grey-primary rounded font-bold
+            ${isInvalid && 'opacity-50'}`}
+          >
+            Login
+          </button>
+        </div>
+        <div className="flex justify-center items-center flex-col p-4 rounded border border-grey-primary">
+          <p className="text-sm">
+            New Account? &ensp;
+            <Link to="/signup" className="font-bold text-blue-medium">
+              Sign up
+            </Link>
+          </p>
+        </div>
       </div>
     </div>
   );
--- src/tailwind.config.js
+++ src/tailwind.config.js
@@ -1,5 +1,29 @@
 module.export = {
     future: {
         removeDeprecatedGapUtilities: true
+    },
+
+    theme: {
+        fill: (theme) => ({
+            red: theme('color.red.primary')
+        })
+        color: {
+            white: '#ffffff',
+            blue: {
+                medium: '#005c98'
+            },
+            black: {
+                light: '#005c98',
+                faded: '#00000059'
+            },
+            grey: {
+                base: '#616161'
+                background: '#fafafa'
+                primary: '#dbdbdb'
+            },
+            red: {
+                primary: '#ed4956'
+            }
+        }
     }
 }
