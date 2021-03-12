6215ab48cd90a1d4dff31bebf833834b51f5e86e
Lucian Boatright <lucian.boatright@gmail.com>
2021-03-12 17:48:28 +0000

    save state precheck - not connecting to firbase

--- src/pages/sign-up.js
+++ src/pages/sign-up.js
@@ -4,33 +4,30 @@ import { Link } from 'react-router-dom';
 import * as ROUTES from '../constants/routes';
 import FireBaseContext from '../context/firebase';
 import image1 from '../images/watchbox.jpg';
+import { doesUsernameExist } from '../services/firebase';
 
-export default function Login() {
+export default function SignUp() {
   const history = useHistory;
   const { firebase } = useContext(FireBaseContext);
 
+  const [username, SetUsername] = useState('');
+  const [fullName, setFullName] = useState('');
   const [emailAddress, setEmailAddress] = useState('');
   const [password, setPassword] = useState('');
 
   const [error, setError] = useState('');
   const isInvalid = password === '' || emailAddress === '';
 
-  const handleLogin = async (event) => {
+  const handleSignup = async (event) => {
     event.preventDefault();
 
-    try {
-      await FireBaseContext.auth().signInWithEmailAndPassowrd(emailAddress, password);
-      history.push(ROUTES.DASHBOARD);
-    } catch (error) {
-      // If there is an erroer reset the vcalues to nothing
-      setEmailAddress('');
-      setPassword('');
-      setError(error.message);
-    }
+    const usernamesExists = await doesUsernameExist(username);
+    // try {
+    // } catch (error) {}
   };
 
   useEffect(() => {
-    document.title = 'Login - Watch';
+    document.title = 'Sign Up - Watch';
   }, []);
 
   return (
@@ -45,9 +42,25 @@ export default function Login() {
           </h1>
           {error && <p className="mb-4 text-xs text-red-primary">{error}</p>}
 
-          <form onSubmit={handleLogin} method="POST">
+          <form onSubmit={handleSignup} method="POST">
             <input
-              aria-label="Enter your email address"
+              aria-label="Enter User Name"
+              type="text"
+              placeholder="Username"
+              className="text-sm text-grey-base w-full mr-3 py-5 px-4 h-2 border border-grey-primary rounded mb-2"
+              onChange={({ target }) => SetUsername(target.value)}
+              value={username}
+            />
+            <input
+              aria-label="Enter your Full Name"
+              type="text"
+              placeholder="Full Name"
+              className="text-sm text-grey-base w-full mr-3 py-5 px-4 h-2 border border-grey-primary rounded mb-2"
+              onChange={({ target }) => setFullName(target.value)}
+              value={fullName}
+            />
+            <input
+              aria-label="Enter your Email Address"
               type="text"
               placeholder="Email Address"
               className="text-sm text-grey-base w-full mr-3 py-5 px-4 h-2 border border-grey-primary rounded mb-2"
@@ -69,14 +82,14 @@ export default function Login() {
             className={`bg-blue-500 text-white w-full border border-grey-primary rounded font-bold
             ${isInvalid && 'opacity-50'}`}
           >
-            Login
+            Sign Up
           </button>
         </div>
         <div className="flex justify-center items-center flex-col p-4 rounded border border-grey-primary">
           <p className="text-sm">
-            New Account? &ensp;
-            <Link to={ROUTES.SIGN_UP} className="font-bold text-blue-medium">
-              Sign up
+            Already have an account? &ensp;
+            <Link to={ROUTES.LOGIN} className="font-bold text-blue-medium">
+              Login
             </Link>
           </p>
         </div>
