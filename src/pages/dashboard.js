3a8aa68a9d98c0c69f5846b23c489b827dc804fa
Lucian Boatright <lucian.boatright@gmail.com>
2021-05-06 16:01:11 +0100

    taken out lines for deployment

--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -2,7 +2,6 @@ import { useEffect } from 'react';
 import Header from '../components/header';
 import Sidebar from '../components/sidebar';
 import Timeline from '../components/timeline';
-import backGrid from '../images/borders/wooden_grid_3_long.png';
 
 export default function Dashboard() {
   useEffect(() => {
@@ -12,7 +11,6 @@ export default function Dashboard() {
   return (
     <div className="bg-grey-background">
       <Header />
-      {/* <img className="" height={170} src={backGrid} alt="" /> */}
       <div>
         <div className="grid grid-cols-4 gap-4 justify-between mx-auto ">
           <Timeline />
