b8887441e5cc05cd2a62d1fa43af58c6a628439b
Lucian Boatright <lucian.boatright@gmail.com>
2021-07-13 16:33:17 +0100

    added items to git ignore

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -2,17 +2,9 @@ import Firebase from 'firebase/app';
 import 'firebase/firestore';
 import 'firebase/auth';
 import 'firebase/storage';
+import { config } from './firebase-config';
 
 // import { seedDatabase } from '../seed';
-
-const config = {
-  apiKey: 'AIzaSyC7p3mdGL8qY_vkyRWA994aqMUGUvJhJAM',
-  authDomain: 'mywatchboxweb.firebaseapp.com',
-  projectId: 'mywatchboxweb',
-  storageBucket: 'mywatchboxweb.appspot.com',
-  messagingSenderId: '922169170815',
-  appId: '1:922169170815:web:4a8cf6b43d83ffb23e584a'
-};
 
 const firebase = Firebase.initializeApp(config);
 const db = firebase.firestore();
