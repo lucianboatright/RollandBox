5c36f690524ae9ff111563f6fbb91c6d6d515e45
luke boat <lucian.boatright@gmail.com>
2021-09-10 11:52:09 +0100

    broswer css tweak

--- src/components/post/actions.js
+++ src/components/post/actions.js
@@ -35,39 +35,43 @@ export default function Actions({ docId, totalLikes, likedWatch, watchContent, h
   return (
     <>
       <div className="flex justify-between pt-1 pb-1 mt-0">
-        <div className="flex">
-          <WatchLogo
-            onClick={handleToggleLiked}
-            onKeyDown={(event) => {
-              if (event.key === 'Enter') {
-                handleToggleLiked();
-              }
-            }}
-            className={`w-4 mr-2 select-none cursor-pointer ${
-              toggleLiked ? 'fill-current text-red-600' : 'text-black-light'
-            }`}
-          />
-          <svg
-            onClick={handleFocus}
-            onKeyDown={(event) => {
-              if (event.key === 'Enter') {
-                handleFocus();
-              }
-            }}
-            className="w-4 text-black-light select-none cursor-pointer"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-            tabIndex={0}
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth={2}
-              d="M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z"
+        <div className="flex items-center">
+          <div>
+            <WatchLogo
+              onClick={handleToggleLiked}
+              onKeyDown={(event) => {
+                if (event.key === 'Enter') {
+                  handleToggleLiked();
+                }
+              }}
+              className={`w-4 mr-2 select-none cursor-pointer ${
+                toggleLiked ? 'fill-current text-red-600' : 'text-black-light'
+              }`}
             />
-          </svg>
+          </div>
+          <div>
+            <svg
+              onClick={handleFocus}
+              onKeyDown={(event) => {
+                if (event.key === 'Enter') {
+                  handleFocus();
+                }
+              }}
+              className="w-4 text-black-light select-none cursor-pointer"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              tabIndex={0}
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                d="M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z"
+              />
+            </svg>
+          </div>
           <div>
             <Magnifying
               onClick={viewWatch}
