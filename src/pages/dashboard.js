d4f1b91ebfdd5aaa65e4524878004a54981bb6e7
luke boat <lucian.boatright@gmail.com>
2021-10-11 18:22:33 +0100

    changed back

--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -5,30 +5,23 @@ import Timeline from '../components/timeline';
 import BottomBanner from '../components/bottomBanner';
 
 export default function Dashboard() {
+  useEffect(() => {
+    document.title = 'RollandBox';
+  }, []);
   const refresh = window.localStorage.getItem('refresh');
   console.log(refresh);
   if (refresh === null) {
     window.location.reload();
     window.localStorage.setItem('refresh', '1');
   }
-  useEffect(() => {
-    document.title = 'RollandBox';
-  }, []);
 
   return (
     <div className="bg-grey-background">
       <Header />
       <div>
-        <div
-          style={{ display: 'grid', gridTemplateColumns: '80% 20%' }}
-          className="mx-auto ml-3 mr-24 sm:ml-10 mr-10 md:ml-10 lg:ml-10 xl:ml-10"
-        >
-          <div>
-            <Timeline />
-          </div>
-          <div>
-            <Sidebar />
-          </div>
+        <div className="grid grid-cols-6 gap-4 justify-between mx-auto ml-3 mr-24 sm:ml-10 mr-10 md:ml-10 lg:ml-10 xl:ml-10">
+          <Timeline />
+          <Sidebar />
         </div>
       </div>
       <BottomBanner />
