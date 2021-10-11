0ce45ac609bbd570588002444897bd669f3dbebb
luke boat <lucian.boatright@gmail.com>
2021-10-11 18:20:05 +0100

    CHANGED MADE TO GRID THAT WORK BUT STILL HAVE ISSUES

--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -5,6 +5,12 @@ import Timeline from '../components/timeline';
 import BottomBanner from '../components/bottomBanner';
 
 export default function Dashboard() {
+  const refresh = window.localStorage.getItem('refresh');
+  console.log(refresh);
+  if (refresh === null) {
+    window.location.reload();
+    window.localStorage.setItem('refresh', '1');
+  }
   useEffect(() => {
     document.title = 'RollandBox';
   }, []);
@@ -13,9 +19,16 @@ export default function Dashboard() {
     <div className="bg-grey-background">
       <Header />
       <div>
-        <div className="grid grid-cols-6 gap-4 justify-between mx-auto ml-3 mr-24 sm:ml-10 mr-10 md:ml-10 lg:ml-10 xl:ml-10">
-          <Timeline />
-          <Sidebar />
+        <div
+          style={{ display: 'grid', gridTemplateColumns: '80% 20%' }}
+          className="mx-auto ml-3 mr-24 sm:ml-10 mr-10 md:ml-10 lg:ml-10 xl:ml-10"
+        >
+          <div>
+            <Timeline />
+          </div>
+          <div>
+            <Sidebar />
+          </div>
         </div>
       </div>
       <BottomBanner />
