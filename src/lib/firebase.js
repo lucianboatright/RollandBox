78f6a46fde95705f07479cb3ddcd45e688d5f564
luke boat <lucian.boatright@gmail.com>
2021-10-16 11:07:19 +0100

    added google analytics

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -3,10 +3,17 @@ import 'firebase/firestore';
 import 'firebase/auth';
 import 'firebase/storage';
 import { config } from './firebase-config';
+import { getAnalytics } from "firebase/analytics";
+import { initializeApp } from "firebase/app";
+
+
 
 const firebase = Firebase.initializeApp(config);
 const db = firebase.firestore();
 const { FieldValue } = Firebase.firestore;
 const { storage } = Firebase.storage();
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
 
 export { firebase, storage, db, FieldValue };
