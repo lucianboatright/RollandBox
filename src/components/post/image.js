a165c24848a5019ef5c073c5edc156c6086c34b0
luke boat <lucian.boatright@gmail.com>
2021-11-13 16:02:22 +0000

    change in css boxes

--- src/components/post/image.js
+++ src/components/post/image.js
@@ -2,13 +2,18 @@ import PropTypes from 'prop-types';
 
 export default function Image({ src, caption }) {
   return (
-    <img
-      className=" mt-6 ml-2.5 align-center rounded"
-      src={src}
-      alt={caption}
-      width={195}
-      height="auto"
-    />
+    <div>
+      <img
+        className=" mt-5 ml-3 align-center rounded"
+        src={src}
+        alt={caption}
+        width={195}
+        height="auto"
+      />
+      <div
+        style={{ height: '300px', width: '195px', boxShadow: 'inset 0px 0px 10px rgba(0,0,0,0.9)' }}
+      />
+    </div>
   );
 }
 
