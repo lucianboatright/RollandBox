e78dfefa3287506ca443e5065a5cf4456672d803
luke boat <lucian.boatright@gmail.com>
2022-01-03 17:07:40 +0000

    added setting to testing setup

--- test/test.js
+++ test/test.js
@@ -8,9 +8,9 @@ describe("Testing on Watch App", () => {
     it("test set up is working", () => {
         assert.equal(2+2, 4)
     })
-    // it("Can read items from Read only Collection", () => {
-    //     const db = firebase.initializeTestApp({projectId: MY_PROJECT_ID}).firestore();
-    //     const testDoc = db.collection("readonly").doc("testDoc");
-    //     await firebase.assertSucceeds(testDoc.get());
-    // })
+    it("Can read items from Read only Collection", async () => {
+        const db = firebase.initializeTestApp({projectId: MY_PROJECT_ID}).firestore();
+        const testDoc = db.collection("readonly").doc("testDoc");
+        await firebase.assertSucceeds(testDoc.get());
+    })
 })
