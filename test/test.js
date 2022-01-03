4b0478a48db74a039daeb752c5e8695291e37a32
luke boat <lucian.boatright@gmail.com>
2022-01-03 18:01:17 +0000

    building second test

--- test/test.js
+++ test/test.js
@@ -8,9 +8,16 @@ describe("Testing on Watch App", () => {
     it("test set up is working", () => {
         assert.equal(2+2, 4)
     })
+
     it("Can read items from Read only Collection", async () => {
         const db = firebase.initializeTestApp({projectId: MY_PROJECT_ID}).firestore();
         const testDoc = db.collection("readonly").doc("testDoc");
         await firebase.assertSucceeds(testDoc.get());
     })
+
+    it("can write to files in read only collection"), async () => {
+        const db = firebase.initializeTestApp({projectId: MY_PROJECT_ID}).firestore();
+        const testDoc = bd.collection("readonly").doc("testDoc2")
+        await firebase.assertSucceeds(testDoc.set({foo: "bar"}));
+    }
 })
